b04f4ec5447d13032eca81c493546dda5bd2e021
Le Quang Khai IUH <khaiquangletn@gmail.com>
2023-02-02 17:43:15 +0700

    Ok connect Redis

--- src/database/init.redis.js
+++ src/database/init.redis.js
@@ -1,7 +1,7 @@
 import Redis from 'ioredis'
 
 const client = async () => {
-    // Connect to Redis at 127.0.0.1, port 6379.
+
     const redisClient = new Redis({
         url: process.env.REDIS_URI
     });
